e56e893c988c8c806133ebfd0fa7f48ef48ac2b8
Julian Lenz <j.lenz@hzdr.de>
2024-11-19 21:19:04 +0100

    resetfreedpages=false

--- frameworks/mallocMC/Instance.cuh
+++ frameworks/mallocMC/Instance.cuh
@@ -21,6 +21,7 @@ struct FlatterScatterHeapConfig : FlatterScatter<>::Properties::HeapConfig {
   // Only used by original Scatter (but it doesn't hurt FlatterScatter to keep):
   static constexpr auto regionsize = 16;
   static constexpr auto wastefactor = wasteFactor;
+  static constexpr bool resetfreedpages = false;
 };
 
 struct ShrinkConfig {
