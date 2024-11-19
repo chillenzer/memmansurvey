9deebb28f02ce1c4b6c2d4eb7705ac397475df34
Julian Lenz <j.lenz@hzdr.de>
2024-11-19 11:40:36 +0100

    Add download_and_run_only_mallocMC.sh

---
+++ install_scripts/download_and_run_only_mallocMC.sh
@@ -0,0 +1,28 @@
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied - architecture must be specified"
+  exit 1
+fi
+
+echo "Building for architecture $1"
+
+echo "Updating submodules"
+git submodule init
+git submodule update
+
+./install_scripts/graph_curl.sh
+
+echo "Downloads complete, compiling"
+mkdir -p build
+yes | python3 init.py
+python3 cleanAll.py
+python3 setupAll.py --cc $1
+mkdir results
+
+echo "Download finished, running experiments"
+
+python3 testMallocMC.py -mem_size 8 -device 0 -runtest -genres
+
+echo "Building PDF"
+
+./install_scripts/process_results.sh
+
