4a2de2957a8a215794b7d9756785601a04787d0d
Julian Lenz <j.lenz@hzdr.de>
2025-05-09 10:46:21 +0200

    Remove explicit dependency on alpaka

--- addAlpaka.cmake
+++ addAlpaka.cmake
@@ -3,8 +3,3 @@ option(alpaka_ACC_GPU_CUDA_ONLY_MODE "" ON)
 set(alpaka_CXX_STANDARD 20)
 set(alpaka_BUILD_EXAMPLES OFF)
 set(BUILD_TESTING OFF)
-
-add_subdirectory(
-  ${BASE_PATH}frameworks/mallocMC/mallocMC/alpaka
- "${CMAKE_CURRENT_BINARY_DIR}/alpaka" EXCLUDE_FROM_ALL)
-
