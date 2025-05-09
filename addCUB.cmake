d904b31a9fba1e282b0e8cc6c66689c93eff59d9
Julian Lenz <j.lenz@hzdr.de>
2025-05-09 11:07:45 +0200

    Fix addCUB

--- addCUB.cmake
+++ addCUB.cmake
@@ -1,5 +1,5 @@
 # Find the CUB library
-find_package(CUB REQUIRED QUIET)
+find_package(CUB QUIET)
 # If the library is not found, use the shipped version
 if(NOT CUB_FOUND)
   message(STATUS "CUB not found, using shipped version. This might lead to incompatibilities.")
