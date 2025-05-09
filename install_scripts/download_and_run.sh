67cbbcbd98e55e90dcd0d2c4756774180b25752b
Julian Lenz <j.lenz@hzdr.de>
2025-05-09 22:25:38 +0200

    Better logging

--- install_scripts/download_and_run.sh
+++ install_scripts/download_and_run.sh
@@ -1,29 +1,46 @@
+set -x
+
 if [ $# -eq 0 ]; then
   echo "No arguments supplied - architecture must be specified"
   exit 1
 fi
 
-echo "Building for architecture $1"
+dt=$(date +"%Y-%m-%d-%H:%M:%S")
+filename=log_${dt}.txt
 
-echo "Updating submodules"
-git submodule init
-git submodule update
+function main() {
+  # We'll gather some information upfront:
+  echo Running on ${dt} on $(hostname)
+  git remote -v
+  git log | head -n 1
+  git status
+  git diff
+  nvidia-smi
+  lscpu
 
-if [[ ! -f ./tests/graph_tests/data/orkut.mtx ]]; then
-  ./install_scripts/graph_curl.sh
-fi
+  echo "Building for architecture $1"
+  echo "Updating submodules"
+  git submodule init
+  git submodule update
 
-echo "Downloads complete, compiling"
-mkdir -p build
-yes | python3 init.py
-python3 cleanAll.py
-python3 setupAll.py --cc $1
-mkdir -p results
+  if [[ ! -f ./tests/graph_tests/data/orkut.mtx ]]; then
+    ./install_scripts/graph_curl.sh
+  fi
 
-echo "Download finished, running experiments"
+  echo "Downloads complete, compiling"
+  mkdir -p build
+  yes | python3 init.py
+  python3 cleanAll.py
+  python3 setupAll.py --cc $1
+  mkdir -p results
 
-yes | python3 testAll.py -mem_size 8 -device 0 -runtest -genres
+  echo "Download finished, running experiments"
 
-echo "Building PDF"
+  yes | python3 testAll.py -mem_size 8 -device 0 -runtest -genres
 
-./install_scripts/process_results.sh
+  echo "Building PDF"
+
+  ./install_scripts/process_results.sh
+}
+
+main $1 > >(tee -a $filename) 2>&1
